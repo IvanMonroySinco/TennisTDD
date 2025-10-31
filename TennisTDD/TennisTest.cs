2134a4c72105107495f3a4876fa0a9b279dd46d0
Ivan Rene Monroy Santos <ivan.monroy@sinco.com.co>
2025-10-31 14:39:07 -0500

    🟥DebeRetornarFifteenLove_Si_JugadorUnoTieneUnPuntoYJugadorDosTieneCeroPuntos

--- TennisTDD/TennisTest.cs
+++ TennisTDD/TennisTest.cs
@@ -14,6 +14,17 @@ public class TennisTest
         //Assert
         resultado.Should().Be("Love-All");
     }
+
+    [Fact]
+    public void DebeRetornarFifteenLove_Si_JugadorUnoTieneUnPuntoYJugadorDosTieneCeroPuntos()
+    {
+        //Arrange
+        var juego = new TennisScoreCalculator();
+        //Act
+        var resultado = juego.Score(1,0);
+        //Assert
+        resultado.Should().Be("Fifteen-Love");
+    }
 }
 
 public class TennisScoreCalculator {
