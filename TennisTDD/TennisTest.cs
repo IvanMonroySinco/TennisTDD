3b11bfe0dcd7438afb3954dcb1b962771d8ab1e4
Ivan Rene Monroy Santos <ivan.monroy@sinco.com.co>
2025-10-31 14:52:57 -0500

    🟩DebeRetornarThirtyFifteen_Si_JugadorUnoTieneDosPuntosYJugadorDosTieneUnPunto

--- TennisTDD/TennisTest.cs
+++ TennisTDD/TennisTest.cs
@@ -53,6 +53,10 @@ public class TennisScoreCalculator {
     
     public string Score(int player1Points, int player2Points)
     {
+        if (player1Points == 2 && player2Points == 1)
+        {
+            return "Thirty-Fifteen"; 
+        }
         if (player1Points == 1 && player2Points == 1)
         {
             return "Fifteen-All"; 
